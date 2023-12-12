615e166f429b1b03acfcc578b54b467a76c2ed69
dabin0519 <dadabin0519@naver.com>
2023-12-12 23:50:34 +0900

    resultManager

---
+++ Framework/2023_winapi_framework/ResultManager.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "ResultManager.h"
+#include "TimeMgr.h"
+
+void ResultManager::Init()
+{
+	_maxTimeScore		= 0;
+	_maxMonsterScore	= 0;
+	_monsterScore		= 0;
+	_timeScore			= 0;
+	_playTime			= true;
+}
+
+void ResultManager::Update()
+{
+	if (_monsterScore > _maxMonsterScore)
+	{
+		_maxMonsterScore = _monsterScore;
+	}
+
+	if (_timeScore > _maxTimeScore)
+	{
+		_maxTimeScore = _timeScore;
+	}
+
+	if(_playTime)
+		_timeScore += DeltaTime;
+}
